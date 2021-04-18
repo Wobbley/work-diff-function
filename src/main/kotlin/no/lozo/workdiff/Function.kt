5fe50e7a334067e059b80fd4ea75ba56d5d0eb23
Dean Lozo <dean.lozo@gmail.com>
2021-04-18 23:05:08 +0200

    Added Proxy Gateway

--- src/main/kotlin/no/lozo/workdiff/Function.kt
+++ src/main/kotlin/no/lozo/workdiff/Function.kt
@@ -2,35 +2,44 @@ package no.lozo.workdiff
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import com.beust.klaxon.Klaxon
 import no.lozo.workdiff.clockify.ClockifyClient
 import no.lozo.workdiff.dto.HandlerInput
 import no.lozo.workdiff.dto.HandlerOutput
+import java.lang.RuntimeException
 import java.time.LocalDate
 import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.toDuration
 
-
 @ExperimentalTime
-class Function : RequestHandler<HandlerInput, HandlerOutput> {
+class Function : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    override fun handleRequest(input: HandlerInput?, context: Context?): HandlerOutput {
-        input?.let {
-            val start = LocalDate.parse(it.startDate)
-            val end = LocalDate.parse(it.endDate)
+    override fun handleRequest(event: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
+        try {
+            val input = Klaxon().parse<HandlerInput>(event.body)!!
+            val start = LocalDate.parse(input.startDate)
+            val end = LocalDate.parse(input.endDate)
 
             val loggedSeconds =
-                ClockifyClient.getSummaryReport(it.workspaceId, it.apiKey, start, end).toDuration(DurationUnit.SECONDS)
+                ClockifyClient.getSummaryReport(input.workspaceId, input.apiKey, start, end).toDuration(DurationUnit.SECONDS)
             val workingDays = calculateWorkDays(start, end)
-            val workingSeconds = workingDays.times(it.hoursInWorkday).times(60).times(60).toDuration(DurationUnit.SECONDS)
+            val workingSeconds = workingDays.times(input.hoursInWorkday).times(60).times(60).toDuration(DurationUnit.SECONDS)
             val diffSeconds = loggedSeconds.minus(workingSeconds)
 
-            return HandlerOutput(
+            val output = HandlerOutput(
                 loggedSeconds.inHours,
                 workingSeconds.inHours,
                 diffSeconds.inHours
             )
+
+            val headers = mapOf(Pair("Access-Control-Allow-Origin", "*"))
+            return APIGatewayProxyResponseEvent().withBody(Klaxon().toJsonString(output)).withHeaders(headers)
+
+        } catch (e: RuntimeException) {
+            return APIGatewayProxyResponseEvent().withStatusCode(500).withBody(e.stackTraceToString())
         }
-        return HandlerOutput(0.0, 0.0, 0.0)
     }
 }
